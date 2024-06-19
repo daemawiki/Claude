024c6b1930015d6926348cca0f715a7ab941176e
101 <tmddnjs001133@dsm.hs.kr>
2024-06-19 20:17:50 +0900

    init :: project

---
+++ src/main/java/com/daemawiki/daemawiki/Application.java
@@ -0,0 +1,13 @@
+package com.daemawiki.daemawiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudeApplication.class, args);
+	}
+
+}
