5fc498dca8f5cd4de677f5e36e69ecfbfb9a04af
101 <tmddnjs001133@dsm.hs.kr>
2024-06-20 22:23:45 +0900

    add :: AuthUserRepositoryImpl.java

---
+++ src/main/java/com/daemawiki/daemawiki/domain/mail/auth_user/repository/AuthUserRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.daemawiki.daemawiki.domain.mail.auth_user.repository;
+
+import com.daemawiki.daemawiki.domain.mail.auth_user.model.AuthUserModel;
+import com.daemawiki.daemawiki.global.utils.redis.RedisKey;
+import com.daemawiki.daemawiki.global.utils.redis.RedisOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Repository
+@RequiredArgsConstructor
+public class AuthUserRepositoryImpl implements AuthUserRepository {
+    @Override
+    public Mono<Boolean> save(AuthUserModel model) {
+        return redisOperation.save(
+                AUTH_MAIL + model.email(),
+                model.email(),
+                Duration.ofHours(3)
+        );
+    }
+
+    @Override
+    public Mono<Boolean> findByMail(String mail) {
+        return redisOperation.getValue(AUTH_MAIL + mail)
+                .hasElement();
+    }
+
+    @Override
+    public Mono<Void> delete(String mail) {
+        return redisOperation.delete(AUTH_MAIL + mail);
+    }
+
+    private static final String AUTH_MAIL = RedisKey.AUTH_USER.getKey();
+    private final RedisOperation redisOperation;
+}
