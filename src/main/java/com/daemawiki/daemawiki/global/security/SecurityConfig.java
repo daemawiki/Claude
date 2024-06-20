cc51c41a831c0649676facaa4b368fc21ed2745b
101 <tmddnjs001133@dsm.hs.kr>
2024-06-20 16:52:51 +0900

    add :: SecurityConfig.java

---
+++ src/main/java/com/daemawiki/daemawiki/global/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.daemawiki.daemawiki.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http.formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchange -> authorizeExchange
+                        .pathMatchers("/api/auth/**").permitAll()
+                        .anyExchange().authenticated())
+                .build();
+    }
+}
