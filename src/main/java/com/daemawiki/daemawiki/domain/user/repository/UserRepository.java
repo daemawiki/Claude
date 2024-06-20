8bd30ea9ba12ad5f3fd4f9d723031348b548c308
101 <tmddnjs001133@dsm.hs.kr>
2024-06-20 20:11:35 +0900

    add :: UserRepository.java

---
+++ src/main/java/com/daemawiki/daemawiki/domain/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.daemawiki.daemawiki.domain.user.repository;
+
+import com.daemawiki.daemawiki.domain.user.model.UserEntity;
+import com.daemawiki.daemawiki.global.utils.PagingInfo;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository {
+    Mono<UserEntity> save(UserEntity entity);
+    Flux<UserEntity> findByGenerationAndMajor(Integer generation, String major, PagingInfo pagingInfo);
+
+}
