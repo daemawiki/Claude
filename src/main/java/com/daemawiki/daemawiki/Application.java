71cf1d7ecaf73cb0a94d089c3c420a0a95c2091c
101 <tmddnjs001133@dsm.hs.kr>
2024-06-20 10:14:07 +0900

    update :: Application.java
    add ConfigurationPropertiesScan annotation

--- src/main/java/com/daemawiki/daemawiki/Application.java
+++ src/main/java/com/daemawiki/daemawiki/Application.java
@@ -2,10 +2,12 @@ package com.daemawiki.daemawiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @EnableWebFluxSecurity
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
 
 	public static void main(String[] args) {
