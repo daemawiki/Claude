9c6c7db28de4c78ee35b9d4e7930ac68f91746c4
101 <tmddnjs001133@dsm.hs.kr>
2024-06-20 22:22:29 +0900

    add :: RedisOperation.java

---
+++ src/main/java/com/daemawiki/daemawiki/global/utils/redis/RedisOperation.java
@@ -0,0 +1,12 @@
+package com.daemawiki.daemawiki.global.utils.redis;
+
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+public interface RedisOperation {
+    Mono<String> getValue(String key);
+    Mono<Void> delete(String key);
+    Mono<Boolean> save(String key, String value, Duration expiration);
+
+}
