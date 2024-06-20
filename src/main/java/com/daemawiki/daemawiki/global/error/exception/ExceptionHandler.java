30d1144a982742c86fcdb2c68cd6161e745ea476
101 <tmddnjs001133@dsm.hs.kr>
2024-06-20 10:13:39 +0900

    add :: ExceptionHandler.java

---
+++ src/main/java/com/daemawiki/daemawiki/global/error/exception/ExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.daemawiki.daemawiki.global.error.exception;
+
+import com.daemawiki.daemawiki.global.error.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import reactor.core.publisher.Mono;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(CustomException.class)
+    public Mono<ResponseEntity<ErrorResponse>> handleCustomException(CustomException e, ServerRequest request) {
+        return Mono.justOrEmpty(
+                ErrorResponse.of(e, request)
+        );
+    }
+}
