c404976629c32009003885d4c4dd27add7c1478e
101 <tmddnjs001133@dsm.hs.kr>
2024-06-20 20:27:58 +0900

    add :: RedisConfig.java

---
+++ src/main/java/com/daemawiki/daemawiki/global/utils/redis/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.daemawiki.daemawiki.global.utils.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @Primary
+    public ReactiveRedisOperations<String, String> redisOperations(ReactiveRedisConnectionFactory factory) {
+        RedisSerializer<String> serializer = new StringRedisSerializer();
+        RedisSerializationContext<String, String> serializationContext = RedisSerializationContext
+                .<String, String>newSerializationContext()
+                .key(serializer)
+                .value(serializer)
+                .hashKey(serializer)
+                .hashValue(serializer)
+                .build();
+
+        return new ReactiveRedisTemplate<>(factory, serializationContext);
+    }
+}
