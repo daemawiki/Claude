dc1f1c286834858b1f6cb48c4fe092e1a0670151
101 <tmddnjs001133@dsm.hs.kr>
2024-06-22 20:05:55 +0900

    update :: UserRepository

--- src/main/java/com/daemawiki/daemawiki/domain/user/repository/UserAbstractRepository.java
+++ src/main/java/com/daemawiki/daemawiki/domain/user/repository/UserAbstractRepository.java
@@ -1,14 +1,27 @@
 package com.daemawiki.daemawiki.domain.user.repository;
 
 import com.daemawiki.daemawiki.domain.user.model.UserEntity;
+import com.daemawiki.daemawiki.domain.user.model.UserModel;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 public abstract class UserAbstractRepository implements UserRepository {
     @Override
-    public Mono<UserEntity> save(UserEntity entity) {
-        return userMongoRepository.save(entity);
+    public Mono<UserModel> save(UserEntity entity) {
+        return userMongoRepository.save(entity)
+                .map(UserModel.class::cast);
+    }
+
+    @Override
+    public Mono<UserModel> findByEmail(String email) {
+        return userMongoRepository.findByEmail(email)
+                .map(UserModel.class::cast);
+    }
+
+    @Override
+    public Mono<Boolean> existsByEmail(String email) {
+        return userMongoRepository.existsByEmail(email);
     }
 
     private final UserMongoRepository userMongoRepository;
--- src/main/java/com/daemawiki/daemawiki/domain/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/daemawiki/daemawiki/domain/user/repository/UserRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.daemawiki.daemawiki.domain.user.repository;
 
 import com.daemawiki.daemawiki.domain.user.model.UserEntity;
+import com.daemawiki.daemawiki.domain.user.model.UserModel;
 import com.daemawiki.daemawiki.global.utils.mongo.MongoQueryUtils;
 import com.daemawiki.daemawiki.global.utils.PagingInfo;
 import org.springframework.data.domain.Sort;
@@ -14,11 +15,11 @@ import java.util.Optional;
 @Repository
 public class UserRepositoryImpl extends UserAbstractRepository {
     @Override
-    public Flux<UserEntity> findByGenerationAndMajor(Integer generation, String major, PagingInfo pagingInfo) {
+    public Flux<UserModel> findByGenerationAndMajor(Integer generation, String major, PagingInfo pagingInfo) {
         return mongoQueryUtils.find(
                 findByGenerationAndMajorQuery(generation, major, pagingInfo),
                 UserEntity.class
-        );
+        ).map(UserModel.class::cast);
     }
 
     private Query findByGenerationAndMajorQuery(Integer generation, String major, PagingInfo pagingInfo) {
