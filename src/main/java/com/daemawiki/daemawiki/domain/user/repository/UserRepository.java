1b946be45c7f200a9d5abd54cd82faaef2487f06
101 <tmddnjs001133@dsm.hs.kr>
2024-06-22 20:05:21 +0900

    update :: UserRepository.java
    findByEmail, existsByEmail 추가

--- src/main/java/com/daemawiki/daemawiki/domain/user/repository/UserRepository.java
+++ src/main/java/com/daemawiki/daemawiki/domain/user/repository/UserRepository.java
@@ -1,12 +1,15 @@
 package com.daemawiki.daemawiki.domain.user.repository;
 
 import com.daemawiki.daemawiki.domain.user.model.UserEntity;
+import com.daemawiki.daemawiki.domain.user.model.UserModel;
 import com.daemawiki.daemawiki.global.utils.PagingInfo;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository {
-    Mono<UserEntity> save(UserEntity entity);
-    Flux<UserEntity> findByGenerationAndMajor(Integer generation, String major, PagingInfo pagingInfo);
+    Mono<UserModel> save(UserEntity entity);
+    Mono<UserModel> findByEmail(String email);
+    Flux<UserModel> findByGenerationAndMajor(Integer generation, String major, PagingInfo pagingInfo);
 
+    Mono<Boolean> existsByEmail(String email);
 }
