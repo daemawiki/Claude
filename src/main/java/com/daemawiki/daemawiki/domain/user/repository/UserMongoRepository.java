024740ab8dfe557962018b38e1dafbb6bb80dc83
101 <tmddnjs001133@dsm.hs.kr>
2024-06-22 20:05:41 +0900

    update :: UserMongoRepository.java

--- src/main/java/com/daemawiki/daemawiki/domain/user/repository/UserMongoRepository.java
+++ src/main/java/com/daemawiki/daemawiki/domain/user/repository/UserMongoRepository.java
@@ -2,6 +2,10 @@ package com.daemawiki.daemawiki.domain.user.repository;
 
 import com.daemawiki.daemawiki.domain.user.model.UserEntity;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserMongoRepository extends ReactiveMongoRepository<UserEntity, String> {
+    Mono<Boolean> existsByEmail(String email);
+    Mono<UserEntity> findByEmail(String email);
+
 }
