31cf5b78bf822e80394b94c39dc955b832e03f87
101 <tmddnjs001133@dsm.hs.kr>
2024-06-25 15:53:27 +0900

    update :: DocumentEntity.java

--- src/main/java/com/daemawiki/daemawiki/domain/document/model/DocumentEntity.java
+++ src/main/java/com/daemawiki/daemawiki/domain/document/model/DocumentEntity.java
@@ -1,5 +1,8 @@
 package com.daemawiki.daemawiki.domain.document.model;
 
+import com.daemawiki.daemawiki.domain.document.model.detail.DocumentInfo;
+import com.daemawiki.daemawiki.domain.document.model.detail.DocumentType;
+import com.daemawiki.daemawiki.global.utils.EditDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
@@ -13,13 +16,40 @@ public class DocumentEntity {
 
     private String title;
 
-    public static DocumentEntity createEntity(String title) {
-        return new DocumentEntity(title);
+    private DocumentInfo info;
+
+    private Long view;
+
+    private Long version;
+
+    private DocumentType type;
+
+    private EditDateTime dateTime;
+
+    public void updateTitle(String title) {
+        this.title = title;
+    }
+
+    public void increaseView() {
+        this.view++;
+    }
+
+    public void increaseVersion() {
+        this.version++;
+    }
+
+    public static DocumentEntity createEntity(String title, DocumentInfo info, DocumentType type) {
+        return new DocumentEntity(title, info, type);
     }
 
     protected DocumentEntity() {}
 
-    private DocumentEntity(String title) {
+    private DocumentEntity(String title, DocumentInfo info, DocumentType type) {
         this.title = title;
+        this.info = info;
+        this.view = 0L;
+        this.version = 0L;
+        this.type = type;
+        this.dateTime = EditDateTime.getNowInstance();
     }
 }
