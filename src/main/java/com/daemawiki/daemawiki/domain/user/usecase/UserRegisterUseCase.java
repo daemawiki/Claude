e1340ca95e28af957dbc89f0c25517dceda9d178
101 <tmddnjs001133@dsm.hs.kr>
2024-06-25 12:22:54 +0900

    update :: UserRegisterUseCase.java

--- src/main/java/com/daemawiki/daemawiki/domain/user/usecase/UserRegisterUseCase.java
+++ src/main/java/com/daemawiki/daemawiki/domain/user/usecase/UserRegisterUseCase.java
@@ -1,8 +1,9 @@
 package com.daemawiki.daemawiki.domain.user.usecase;
 
+import com.daemawiki.daemawiki.domain.user.dto.UserRegisterRequest;
 import reactor.core.publisher.Mono;
 
 public interface UserRegisterUseCase {
-    Mono<Void> register();
+    Mono<Void> register(UserRegisterRequest request);
 
 }
