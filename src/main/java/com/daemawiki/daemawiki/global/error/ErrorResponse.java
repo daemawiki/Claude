8963150a94aac4da02c6e7148229d5d13eddf52e
101 <tmddnjs001133@dsm.hs.kr>
2024-06-28 17:01:35 +0900

    error :: ErrorResponse.java
    ServerRequest 파라미터 사용 시에 IllegalStateException 발생으로 임시 조치

--- src/main/java/com/daemawiki/daemawiki/global/error/ErrorResponse.java
+++ src/main/java/com/daemawiki/daemawiki/global/error/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.daemawiki.daemawiki.global.error;
 
 import com.daemawiki.daemawiki.global.error.exception.CustomException;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.server.ServerRequest;
 
@@ -10,6 +11,7 @@ public record ErrorResponse(
         int status,
         String message,
         String viewMessage,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "Asia/Seoul")
         LocalDateTime timestamp,
         String path,
         String exception,
@@ -19,12 +21,12 @@ public record ErrorResponse(
         return new ErrorResponse(error.getStatus(), error.getMessage(), error.getViewMessage(), LocalDateTime.now(), path, exception, error.getDetail());
     }
 
-    public static ResponseEntity<ErrorResponse> of(CustomException e, ServerRequest request) {
+    public static ResponseEntity<ErrorResponse> of(CustomException e) {
         var error = e.getError();
         return ResponseEntity.status(error.getStatus())
                 .body(of(
                         error,
-                        request.path(),
+                        "path", // TODO: 6/28/24 ServerRequest 대안 필요. 
                         e.getClass().getSimpleName()
                 ));
     }
