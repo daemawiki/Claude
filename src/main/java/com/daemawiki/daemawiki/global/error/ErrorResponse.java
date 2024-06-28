7760a95694e40dd1d633fb77b9cabf2175b1fb50
101 <tmddnjs001133@dsm.hs.kr>
2024-06-28 15:29:23 +0900

    update :: Error.java

--- src/main/java/com/daemawiki/daemawiki/global/error/ErrorResponse.java
+++ src/main/java/com/daemawiki/daemawiki/global/error/ErrorResponse.java
@@ -9,12 +9,14 @@ import java.time.LocalDateTime;
 public record ErrorResponse(
         int status,
         String message,
+        String viewMessage,
         LocalDateTime timestamp,
         String path,
-        String exception
+        String exception,
+        String detail
 ) {
     private static ErrorResponse of(Error error, String path, String exception) {
-        return new ErrorResponse(error.getStatus(), error.getMessage(), LocalDateTime.now(), path, exception);
+        return new ErrorResponse(error.getStatus(), error.getMessage(), error.getViewMessage(), LocalDateTime.now(), path, exception, error.getDetail());
     }
 
     public static ResponseEntity<ErrorResponse> of(CustomException e, ServerRequest request) {
