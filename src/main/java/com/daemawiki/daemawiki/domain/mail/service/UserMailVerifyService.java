798abe32e416f1039702aeadeb105bc39324ba9d
101 <tmddnjs001133@dsm.hs.kr>
2024-06-29 23:26:01 +0900

    refactor :: UserMailVerifyService.java

--- src/main/java/com/daemawiki/daemawiki/domain/mail/service/UserMailVerifyService.java
+++ src/main/java/com/daemawiki/daemawiki/domain/mail/service/UserMailVerifyService.java
@@ -3,7 +3,6 @@ package com.daemawiki.daemawiki.domain.mail.service;
 import com.daemawiki.daemawiki.domain.mail.auth_code.repository.AuthCodeRepository;
 import com.daemawiki.daemawiki.domain.mail.auth_user.repository.AuthUserRepository;
 import com.daemawiki.daemawiki.domain.mail.usecase.UserMailVerifyUseCase;
-import com.daemawiki.daemawiki.global.error.customs.WrongRedisConnectionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -14,12 +13,14 @@ public class UserMailVerifyService implements UserMailVerifyUseCase {
     @Override
     public Mono<Void> verify(String target, String code) {
         return authCodeRepository.findByMail(target)
-                .switchIfEmpty(Mono.error(new RuntimeException())) // 인증 코드 존재 x
-                .filter(model -> model.code().equals(code))
-                .switchIfEmpty(Mono.error(new RuntimeException())) // 메일 인증 실패
-                .flatMap(model -> authUserRepository.save(model.email()))
-                .then()
-                .onErrorMap(e -> e);
+                .switchIfEmpty(Mono.error(new RuntimeException())) // 인증 내역 존재 x
+                .flatMap(model -> {
+                    if (!model.code().equals(code)) {
+                        return Mono.error(new RuntimeException()); // 인증 실패
+                    }
+                    return authUserRepository.save(model.email());
+                })
+                .then();
     }
 
     private final AuthCodeRepository authCodeRepository;
