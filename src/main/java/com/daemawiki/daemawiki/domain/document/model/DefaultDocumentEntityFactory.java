a177a9c1a28db58a89cc46f2d4652a5507d39bc4
101 <tmddnjs001133@dsm.hs.kr>
2024-07-03 15:37:40 +0900

    update :: DefaultDocumentEntityFactory.java

--- src/main/java/com/daemawiki/daemawiki/domain/document/model/DefaultDocumentEntityFactory.java
+++ src/main/java/com/daemawiki/daemawiki/domain/document/model/DefaultDocumentEntityFactory.java
@@ -1,10 +1,12 @@
 package com.daemawiki.daemawiki.domain.document.model;
 
+import com.daemawiki.daemawiki.domain.document.dto.CreateDocumentRequest;
 import com.daemawiki.daemawiki.domain.document.model.detail.DocumentInfo;
 import com.daemawiki.daemawiki.domain.document.model.detail.DocumentInfoDetail;
 import com.daemawiki.daemawiki.domain.document.model.detail.DocumentType;
 import com.daemawiki.daemawiki.domain.user.model.UserEntity;
 
+import java.util.Collections;
 import java.util.List;
 
 public class DefaultDocumentEntityFactory {
@@ -13,23 +15,40 @@ public class DefaultDocumentEntityFactory {
     private static final String GENERATION_SUFFIX = "기";
     private static final String MAJOR_TITLE = "전공";
 
+    public static DocumentEntity createAnyDocument(CreateDocumentRequest request) {
+        return createDocumentEntity(request, null);
+    }
+
     public static DocumentEntity createStudentDocument(UserEntity user) {
-        return DocumentEntity.createEntity(
-                user.getName(),
-                DocumentInfo.of(
+        return createStudentDocumentEntity(user);
+    }
+
+    private static DocumentEntity createStudentDocumentEntity(UserEntity user) {
+        var generation = user.getUserInfo().generation() + GENERATION_SUFFIX;
+        var major = user.getUserInfo().major();
+
+        var details = List.of(
+                DocumentInfoDetail.of(GENERATION_TITLE, generation),
+                DocumentInfoDetail.of(MAJOR_TITLE, major)
+        );
+
+        return createDocumentEntity(
+                CreateDocumentRequest.of(
                         user.getName(),
-                        List.of(
-                                DocumentInfoDetail.of(
-                                        GENERATION_TITLE,
-                                        user.getUserInfo().generation() + GENERATION_SUFFIX
-                                ),
-                                DocumentInfoDetail.of(
-                                        MAJOR_TITLE,
-                                        user.getUserInfo().major()
-                                )
-                        )
+                        DocumentType.STUDENT,
+                        List.of(generation, major)
+                ), details
+        );
+    }
+
+    private static DocumentEntity createDocumentEntity(CreateDocumentRequest request, List<DocumentInfoDetail> details) {
+        return DocumentEntity.createEntity(
+                request.title(),
+                DocumentInfo.of(
+                        request.title(),
+                        details == null ? Collections.emptyList() : details
                 ),
-                DocumentType.STUDENT
+                request.type()
         );
     }
 }
