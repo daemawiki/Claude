f65b17f5b83213714a97aa7c03ff605e4f9e6661
101 <tmddnjs001133@dsm.hs.kr>
2024-07-07 15:10:38 +0900

    refactor

--- src/main/java/com/daemawiki/daemawiki/domain/user/dto/UserReissueResponse.java
+++
@@ -1,9 +0,0 @@
-package com.daemawiki.daemawiki.domain.user.dto;
-
-public record UserReissueResponse(
-        String token
-) {
-    public static UserReissueResponse of(String token) {
-        return new UserReissueResponse(token);
-    }
-}
--- src/main/java/com/daemawiki/daemawiki/domain/user/service/UserTokenReissueService.java
+++ src/main/java/com/daemawiki/daemawiki/domain/user/service/UserTokenReissueService.java
@@ -1,7 +1,7 @@
 package com.daemawiki.daemawiki.domain.user.service;
 
-import com.daemawiki.daemawiki.domain.user.dto.UserReissueResponse;
-import com.daemawiki.daemawiki.domain.user.usecase.UserReissueUseCase;
+import com.daemawiki.daemawiki.domain.user.dto.UserTokenReissueResponse;
+import com.daemawiki.daemawiki.domain.user.usecase.UserTokenReissueUseCase;
 import com.daemawiki.daemawiki.global.security.token.Tokenizer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,11 +9,11 @@ import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
-public class UserReissueService implements UserReissueUseCase {
+public class UserTokenReissueService implements UserTokenReissueUseCase {
     @Override
-    public Mono<UserReissueResponse> reissue(String token) {
+    public Mono<UserTokenReissueResponse> reissue(String token) {
         return tokenizer.reissue(token)
-                .map(UserReissueResponse::of);
+                .map(UserTokenReissueResponse::of);
     }
 
     private final Tokenizer tokenizer;
