6d36123af9b1ffd410e6fab66cc9a8a2f6f7ad23
101 <tmddnjs001133@dsm.hs.kr>
2024-08-02 15:51:15 +0900

    refactor :: mail send service

--- src/main/java/com/daemawiki/daemawiki/domain/mail/service/UserMailSendService.java
+++ src/main/java/com/daemawiki/daemawiki/domain/mail/service/UserMailSendService.java
@@ -42,40 +42,32 @@ public class UserMailSendService implements UserMailSendUseCase {
     }
 
     private void sendMailInBackground(AuthCodeModel authCodeModel) {
-        sendMail(authCodeModel)
+        Mono.fromRunnable(() -> sendMail(authCodeModel))
+                .subscribeOn(Schedulers.boundedElastic())
+                .onErrorMap(e -> new RuntimeException())
                 .subscribe();
     }
 
-    private Mono<Void> sendMail(AuthCodeModel authCodeModel) {
-        return Mono.fromCallable(() -> {
-                    MimeMessage message = mailSender.createMimeMessage();
-                    MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+    private void sendMail(AuthCodeModel authCodeModel) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-                    helper.setTo(authCodeModel.email());
-                    helper.setSubject(MAIL_TITLE);
-                    helper.setText(getMailTemplate(authCodeModel.code()), true);
-                    helper.setFrom(new InternetAddress(mailSenderProperties.email(), MAIL_TITLE));
+            helper.setTo(authCodeModel.email());
+            helper.setSubject(MAIL_TITLE);
+            helper.setText(String.format(MAIL_TEMPLATE, authCodeModel.code()), true);
+            helper.setFrom(new InternetAddress(mailSenderProperties.email(), MAIL_TITLE));
 
-                    mailSender.send(message);
-                    return null;
-                })
-                .subscribeOn(Schedulers.boundedElastic())
-                .onErrorMap(e -> new RuntimeException())
-                .then();
+            mailSender.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 
     private Mono<AuthCodeModel> saveAuthCode(AuthCodeModel authCodeModel) {
         return authCodeRepository.save(authCodeModel)
                 .thenReturn(authCodeModel)
                 .onErrorMap(e -> e);
-    }
-
-    private String getMailTemplate(String key) {
-        return "<div style='margin: 10px; background-color: #f5f5f5; padding: 20px; border-radius: 10px;'>"
-                + "<p style='font-size: 16px; color: #333;'><b><span style='color: #007bff;'>D</span><span style='color: #ffcc00;'>S</span><span style='color: #ff0000;'>M</span></b> 이메일 인증 코드 :</p>"
-                + "<p style='font-size: 24px; font-weight: bold; color: #007bff; letter-spacing: 3px;'>" + key + "</p>"
-                + "<p style='font-size: 14;font-style: italic; color: #999;'>인증 코드는 30분 동안 유효합니다.</p>"
-                + "</div>";
     }
 
     private String getRandomCode() {
@@ -92,6 +84,12 @@ public class UserMailSendService implements UserMailSendUseCase {
     }
 
     private static final String MAIL_TITLE = "DSM 메일 인증";
+    private static final String MAIL_TEMPLATE =
+            "<div style='margin: 10px; background-color: #f5f5f5; padding: 20px; border-radius: 10px;'>"
+                    + "<p style='font-size: 16px; color: #333;'><b><span style='color: #007bff;'>D</span><span style='color: #ffcc00;'>S</span><span style='color: #ff0000;'>M</span></b> 이메일 인증 코드 :</p>"
+                    + "<p style='font-size: 24px; font-weight: bold; color: #007bff; letter-spacing: 3px;'>%s</p>"
+                    + "<p style='font-size: 14;font-style: italic; color: #999;'>인증 코드는 30분 동안 유효합니다.</p>"
+                    + "</div>";
 
     private final MailSenderProperties mailSenderProperties;
     private final AuthCodeRepository authCodeRepository;
