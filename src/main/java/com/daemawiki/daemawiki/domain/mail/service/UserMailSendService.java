41c9db4c5414a15d53789d3dbbe8def6f579931c
101 <tmddnjs001133@dsm.hs.kr>
2024-08-03 17:17:19 +0900

    add :: UserMailSendService.java

--- src/main/java/com/daemawiki/daemawiki/domain/mail/service/UserMailSendService.java
+++ src/main/java/com/daemawiki/daemawiki/domain/mail/service/UserMailSendService.java
@@ -3,22 +3,15 @@ package com.daemawiki.daemawiki.domain.mail.service;
 import com.daemawiki.daemawiki.domain.mail.auth_code.model.AuthCodeModel;
 import com.daemawiki.daemawiki.domain.mail.auth_code.repository.AuthCodeRepository;
 import com.daemawiki.daemawiki.domain.mail.model.MailType;
+import com.daemawiki.daemawiki.domain.mail.model.event.MailSendEvent;
+import com.daemawiki.daemawiki.domain.mail.usecase.UserMailSendEventHandler;
 import com.daemawiki.daemawiki.domain.mail.usecase.UserMailSendUseCase;
 import com.daemawiki.daemawiki.domain.user.repository.UserRepository;
-import com.daemawiki.daemawiki.global.mail.MailSenderProperties;
 import com.daemawiki.daemawiki.global.utils.AuthCodeGenerator;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
-
-import java.security.SecureRandom;
-import java.util.Base64;
 
 @Service
 @RequiredArgsConstructor
@@ -36,33 +29,11 @@ public class UserMailSendService implements UserMailSendUseCase {
     private Mono<Void> processSendMail(String to) {
         return saveAuthCode(AuthCodeModel.of(to, authCodeGenerator.generate(CODE_LENGTH)))
                 .doOnNext(authCodeModel -> log.info("authCode: {} to: {}", authCodeModel.code(), authCodeModel.email()))
-                .flatMap(authCodeModel -> {
-                    sendMailInBackground(authCodeModel);
-                    return Mono.empty();
-                });
+                .map(authCodeModel -> new MailSendEvent(authCodeModel.email(), String.format(MAIL_TEMPLATE, authCodeModel.code())))
+                .doOnSuccess(userMailSendEventHandler::handleEvent)
+                .then();
     }
 
-    private void sendMailInBackground(AuthCodeModel authCodeModel) {
-        Mono.fromRunnable(() -> sendMail(authCodeModel))
-                .subscribeOn(Schedulers.boundedElastic())
-                .subscribe();
-    }
-
-    private void sendMail(AuthCodeModel authCodeModel) {
-        try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-
-            helper.setTo(authCodeModel.email());
-            helper.setSubject(MAIL_TITLE);
-            helper.setText(String.format(MAIL_TEMPLATE, authCodeModel.code()), true);
-            helper.setFrom(new InternetAddress(mailSenderProperties.email(), MAIL_TITLE));
-
-            mailSender.send(message);
-        } catch (Exception e) {
-            throw new RuntimeException();
-        }
-    }
 
     private Mono<AuthCodeModel> saveAuthCode(AuthCodeModel authCodeModel) {
         return authCodeRepository.save(authCodeModel)
@@ -71,7 +42,6 @@ public class UserMailSendService implements UserMailSendUseCase {
     }
 
     private static final int CODE_LENGTH = 6;
-    private static final String MAIL_TITLE = "DSM 메일 인증";
     private static final String MAIL_TEMPLATE =
             "<div style='margin: 10px; background-color: #f5f5f5; padding: 20px; border-radius: 10px;'>"
                     + "<p style='font-size: 16px; color: #333;'><b><span style='color: #007bff;'>D</span><span style='color: #ffcc00;'>S</span><span style='color: #ff0000;'>M</span></b> 이메일 인증 코드 :</p>"
@@ -79,9 +49,9 @@ public class UserMailSendService implements UserMailSendUseCase {
                     + "<p style='font-size: 14;font-style: italic; color: #999;'>인증 코드는 30분 동안 유효합니다.</p>"
                     + "</div>";
 
-    private final MailSenderProperties mailSenderProperties;
+
+    private final UserMailSendEventHandler userMailSendEventHandler;
     private final AuthCodeRepository authCodeRepository;
     private final AuthCodeGenerator authCodeGenerator;
     private final UserRepository userRepository;
-    private final JavaMailSender mailSender;
 }
