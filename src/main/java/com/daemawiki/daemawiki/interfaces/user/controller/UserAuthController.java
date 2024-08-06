f36787dd9a4dd6082a7deaea327fe2b0832bb4b1
101 <tmddnjs001133@dsm.hs.kr>
2024-08-06 19:43:43 +0900

    update :: UserAuthController.java
    secure 허용, reissue api 삭제

--- src/main/java/com/daemawiki/daemawiki/interfaces/user/controller/UserAuthController.java
+++ src/main/java/com/daemawiki/daemawiki/interfaces/user/controller/UserAuthController.java
@@ -2,18 +2,17 @@ package com.daemawiki.daemawiki.interfaces.user.controller;
 
 import com.daemawiki.daemawiki.interfaces.user.dto.UserLoginRequest;
 import com.daemawiki.daemawiki.interfaces.user.dto.UserRegisterRequest;
-import com.daemawiki.daemawiki.interfaces.user.dto.UserTokenReissueResponse;
 import com.daemawiki.daemawiki.application.user.usecase.UserLoginUseCase;
 import com.daemawiki.daemawiki.application.user.usecase.UserRegisterUseCase;
-import com.daemawiki.daemawiki.application.user.usecase.UserTokenReissueUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,22 +31,17 @@ public class UserAuthController {
             ServerHttpResponse serverHttpResponse,
             ServerHttpRequest serverHttpRequest
     ) {
-        return userLoginUseCase.loginSession(request, serverHttpRequest)
+        return userLoginUseCase.loginWithSession(request, serverHttpRequest)
                 .map(sessionValue ->  ResponseCookie.from("sessionId", sessionValue)
                         .path("/api")
                         .httpOnly(true)
-                        .maxAge(60 * 60 * 3L)
+                        .secure(true)
+                        .maxAge(Duration.ofHours(3))
                         .build())
                 .doOnNext(serverHttpResponse::addCookie)
                 .then();
     }
 
-    @PutMapping("/reissue")
-    public Mono<UserTokenReissueResponse> reissue(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
-        return userTokenReissueUseCase.reissue(token);
-    }
-
     private final UserRegisterUseCase userRegisterUseCase;
-    private final UserTokenReissueUseCase userTokenReissueUseCase;
     private final UserLoginUseCase userLoginUseCase;
 }
