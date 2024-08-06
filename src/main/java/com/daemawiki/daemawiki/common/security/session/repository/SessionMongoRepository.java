3931e5456937bc6f8b9e483cbb6b34dcab0d4b9f
101 <tmddnjs001133@dsm.hs.kr>
2024-08-06 19:43:03 +0900

    delete :: 세션 Mongo 저장소 삭제

--- src/main/java/com/daemawiki/daemawiki/common/security/session/repository/SessionMongoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.daemawiki.daemawiki.common.security.session.repository;
-
-import com.daemawiki.daemawiki.common.security.session.model.SessionEntity;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
-
-public interface SessionMongoRepository extends ReactiveMongoRepository<SessionEntity, String> {
-    Mono<SessionEntity> findByIdAndIp(String sessionId, String ip);
-}
