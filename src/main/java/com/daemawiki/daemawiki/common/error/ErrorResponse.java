f25468e8de9fff5bf41e3643fa70fac6a2b61125
Daybreak312 <ty82afg12@gmail.com>
2024-08-09 11:14:56 +0900

    fix :: 서버 에러 메세지 노출 차단

--- src/main/java/com/daemawiki/daemawiki/common/error/ErrorResponse.java
+++ src/main/java/com/daemawiki/daemawiki/common/error/ErrorResponse.java
@@ -3,7 +3,6 @@ package com.daemawiki.daemawiki.common.error;
 import com.daemawiki.daemawiki.common.error.exception.CustomException;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
 
@@ -13,12 +12,20 @@ public record ErrorResponse(
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "Asia/Seoul")
         LocalDateTime timestamp
 ) {
+    public ErrorResponse {
+        final var httpStatus = HttpStatus.valueOf(status());
+
+        if (httpStatus.is5xxServerError()) {
+            message = httpStatus.name();
+        }
+    }
+
     public static ErrorResponse ofCustomException(CustomException e) {
         return new ErrorResponse(
-                        e.getStatus().value(),
-                        e.getMessage(),
-                        LocalDateTime.now()
-                );
+                e.getStatus().value(),
+                e.getMessage(),
+                LocalDateTime.now()
+        );
     }
 
     public static ErrorResponse ofSecurityError(HttpStatus status, String message) {
