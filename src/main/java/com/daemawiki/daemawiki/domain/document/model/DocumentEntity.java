4f74f9927b2677ac6d87aba5aca7a1a7eff32304
101 <tmddnjs001133@dsm.hs.kr>
2024-08-13 19:01:46 +0900

    update: change document's field to value objects

--- src/main/java/com/daemawiki/daemawiki/domain/document/model/DocumentEntity.java
+++ src/main/java/com/daemawiki/daemawiki/domain/document/model/DocumentEntity.java
@@ -1,13 +1,8 @@
 package com.daemawiki.daemawiki.domain.document.model;
 
-import com.daemawiki.daemawiki.interfaces.document.dto.request.UpdateDocumentInfoAndCategoryRequest;
-import com.daemawiki.daemawiki.domain.document.model.detail.DocumentContent;
-import com.daemawiki.daemawiki.domain.document.model.detail.DocumentEditor;
-import com.daemawiki.daemawiki.domain.document.model.detail.DocumentInfo;
-import com.daemawiki.daemawiki.domain.document.model.detail.DocumentType;
-import com.daemawiki.daemawiki.domain.user.model.UserEntity;
-import com.daemawiki.daemawiki.domain.user.model.detail.UserRole;
+import com.daemawiki.daemawiki.domain.document.model.detail.*;
 import com.daemawiki.daemawiki.common.util.date.EditDateTime;
+import com.daemawiki.daemawiki.domain.user.model.UserEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
@@ -15,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 @Document(collection = "documents")
 @Getter(value = AccessLevel.PUBLIC)
@@ -24,45 +20,45 @@ public class DocumentEntity {
 
     private String title;
 
-    private DocumentInfo info;
+    private Info info;
 
-    private List<DocumentContent> contents;
+    private List<Content> contentList;
 
-    private List<String> category;
+    private Set<String> categoryList;
 
     private Long view;
 
     private Long version;
 
-    private DocumentType type;
+    private Type type;
 
     private EditDateTime dateTime;
 
-    private DocumentEditor owner;
+    private Editor owner;
 
-    private List<DocumentEditor> editors;
+    private List<Editor> editorList;
 
-    public boolean canEdit(UserEntity user) {
-        var editor = DocumentEditor.fromUser(user);
-        return (editors.contains(editor) || isOwner(editor))
-                && (!UserRole.DSM_MOP.equals(user.getRole()));
+    /* setter And more. */
+
+    public boolean canEdit(Editor editor) { // TODO: 8/13/24 DSM_MOP 검사 로직 옮기기
+        return editorList.contains(editor) || isOwner(editor);
     }
 
-    public boolean isOwner(DocumentEditor editor) {
+    public boolean isOwner(Editor editor) {
         return owner.equals(editor);
     }
 
-    public void updateDocumentInfoAndCategory(UpdateDocumentInfoAndCategoryRequest infoAndCatefory) {
-        this.info = infoAndCatefory.info();
-        this.category = infoAndCatefory.category();
+    public void updateDocumentInfoAndCategory(Info info, Set<String> categoryList) {
+        this.info = info;
+        this.categoryList = categoryList;
     }
 
-    public void updateEditors(List<DocumentEditor> editors) {
-        this.editors = editors;
+    public void updateEditors(List<Editor> editorList) {
+        this.editorList = editorList;
     }
 
-    public void updateContents(List<DocumentContent> contents) {
-        this.contents = contents;
+    public void updateContents(List<Content> contentList) {
+        this.contentList = contentList;
     }
 
     public void updateTitle(String title) {
@@ -77,22 +73,46 @@ public class DocumentEntity {
         this.version++;
     }
 
-    public static DocumentEntity createEntity(String title, DocumentInfo info, List<String> category, DocumentType type, DocumentEditor owner) {
-        return new DocumentEntity(title, info, category, type, owner);
+    /* static factory methods */
+
+    public static DocumentEntity createEntity(String title, Info info, Set<String> categoryList, Type type, Editor owner) {
+        return new DocumentEntity(title, info, categoryList, type, owner);
     }
+
+    /* constructors */
 
     protected DocumentEntity() {}
 
-    private DocumentEntity(String title, DocumentInfo info, List<String> category, DocumentType type, DocumentEditor owner) {
+    private DocumentEntity(String title, Info info, Set<String> categoryList, Type type, Editor owner) {
         this.title = title;
         this.info = info;
-        this.contents = Collections.emptyList();
-        this.category = category;
+        this.contentList = Collections.emptyList();
+        this.categoryList = categoryList;
         this.view = 0L;
         this.version = 0L;
         this.type = type;
-        this.dateTime = EditDateTime.getNowInstance();
+        this.dateTime = EditDateTime.getNewInstance();
         this.owner = owner;
-        this.editors = Collections.emptyList();
+        this.editorList = Collections.emptyList();
+    }
+
+    /* value objects */
+
+    public record Info(String subTitle, List<DocumentInfoDetail> details) {}
+
+    public record Content(String index, String title, String content) {}
+
+    public record Editor(String name, String userId) {
+        public static Editor fromUser(UserEntity user) {
+            return new Editor(user.getName(), user.getId());
+        }
+    }
+
+    public enum Type {
+        STUDENT,
+        MAIN,
+        TEACHER,
+        INCIDENT,
+        TEST
     }
 }
