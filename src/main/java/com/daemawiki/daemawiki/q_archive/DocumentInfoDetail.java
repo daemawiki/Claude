95ff9ebad1962cb15ae425c5f917b6f1f6d50579
101 <tmddnjs001133@dsm.hs.kr>
2024-08-13 19:09:47 +0900

    delete: archive 패키지로 이동
    archive 패키지는 휴지통 개념을 생각하면 됩니다.
    
    삭제할 파일이지만, 내용을 확인하고 복구할 수 있는 그런 패키지입니다.

---
+++ src/main/java/com/daemawiki/daemawiki/q_archive/DocumentInfoDetail.java
@@ -0,0 +1,10 @@
+//package com.daemawiki.daemawiki.domain.document.model.detail;
+//
+//public record DocumentInfoDetail(
+//        String title,
+//        String content
+//) {
+//    public static DocumentInfoDetail of(String title, String content) {
+//        return new DocumentInfoDetail(title, content);
+//    }
+//}
