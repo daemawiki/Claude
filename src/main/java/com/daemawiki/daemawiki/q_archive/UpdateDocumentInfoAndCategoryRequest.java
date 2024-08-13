0ed3b2a0a3bbcf0b2a51a3dfcac8355bfae6f002
101 <tmddnjs001133@dsm.hs.kr>
2024-08-13 19:11:45 +0900

    archive: move archive package

---
+++ src/main/java/com/daemawiki/daemawiki/q_archive/UpdateDocumentInfoAndCategoryRequest.java
@@ -0,0 +1,12 @@
+//package com.daemawiki.daemawiki.interfaces.document.dto.request;
+//
+//import com.daemawiki.daemawiki.domain.document.model.detail.DocumentInfo;
+//
+//import java.util.List;
+//import java.util.Set;
+//
+//public record UpdateDocumentInfoAndCategoryRequest(
+//        DocumentInfo info,
+//        Set<String> category
+//) {
+//}
