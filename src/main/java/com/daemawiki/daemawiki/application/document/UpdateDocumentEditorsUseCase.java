ad074a7e5fef11092389501c17f5027dbc107fbc
101 <tmddnjs001133@dsm.hs.kr>
2024-08-17 01:51:43 +0900

    refactor: mapper & Dto 사용

--- src/main/java/com/daemawiki/daemawiki/application/document/UpdateDocumentEditorsUseCase.java
+++ src/main/java/com/daemawiki/daemawiki/application/document/UpdateDocumentEditorsUseCase.java
@@ -1,11 +1,11 @@
 package com.daemawiki.daemawiki.application.document;
 
-import com.daemawiki.daemawiki.domain.document.model.detail.DocumentEditor;
+import com.daemawiki.daemawiki.domain.document.internal.DocumentEditorInfo;
 import com.daemawiki.daemawiki.application.document.base.DocumentUpdateUseCaseBase;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-public interface UpdateDocumentEditorsUseCase extends DocumentUpdateUseCaseBase<List<DocumentEditor>> {
-    Mono<Void> update(String documentId, List<DocumentEditor> updateData);
+public interface UpdateDocumentEditorsUseCase extends DocumentUpdateUseCaseBase<List<DocumentEditorInfo>> {
+    Mono<Void> update(String documentId, List<DocumentEditorInfo> updateData);
 }
--- src/main/java/com/daemawiki/daemawiki/application/document/service/UpdateDocumentEditorsService.java
+++ src/main/java/com/daemawiki/daemawiki/application/document/service/UpdateDocumentEditorsService.java
@@ -1,8 +1,8 @@
 package com.daemawiki.daemawiki.application.document.service;
 
 import com.daemawiki.daemawiki.application.document.UpdateDocumentEditorsUseCase;
-import com.daemawiki.daemawiki.domain.document.model.DocumentEntity;
-import com.daemawiki.daemawiki.domain.document.model.detail.DocumentEditor;
+import com.daemawiki.daemawiki.domain.document.internal.DocumentEditorInfo;
+import com.daemawiki.daemawiki.domain.document.model.DocumentElementMapper;
 import com.daemawiki.daemawiki.domain.document.repository.DocumentRepository;
 import com.daemawiki.daemawiki.application.document.service.base.AbstractDocumentUpdateServiceBase;
 import com.daemawiki.daemawiki.application.user.component.CurrentUser;
@@ -12,11 +12,15 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @Service
-class UpdateDocumentEditorsService extends AbstractDocumentUpdateServiceBase<List<DocumentEditor>> implements UpdateDocumentEditorsUseCase {
+class UpdateDocumentEditorsService extends AbstractDocumentUpdateServiceBase<List<DocumentEditorInfo>> implements UpdateDocumentEditorsUseCase {
 
     @Override
-    public Mono<Void> update(String documentId, List<DocumentEditor> updateData) {
-        return updateDocument(documentId, updateData, DocumentEntity::updateEditors);
+    public Mono<Void> update(String documentId, List<DocumentEditorInfo> updateData) {
+        return updateDocument(
+                documentId,
+                updateData,
+                (document, editors) -> document.updateEditors(DocumentElementMapper.toEditorList(editors))
+        );
     }
 
     public UpdateDocumentEditorsService(DocumentRepository documentRepository, CurrentUser currentUser) {
