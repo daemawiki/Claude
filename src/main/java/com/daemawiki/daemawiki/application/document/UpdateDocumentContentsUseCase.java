139edbeb6b9c5408aaa0094eaf6f65529a4beb88
101 <tmddnjs001133@dsm.hs.kr>
2024-08-17 01:51:39 +0900

    refactor: mapper & Dto 사용

--- src/main/java/com/daemawiki/daemawiki/application/document/UpdateDocumentContentsUseCase.java
+++ src/main/java/com/daemawiki/daemawiki/application/document/UpdateDocumentContentsUseCase.java
@@ -2,10 +2,11 @@ package com.daemawiki.daemawiki.application.document;
 
 import com.daemawiki.daemawiki.domain.document.model.detail.DocumentContent;
 import com.daemawiki.daemawiki.application.document.base.DocumentUpdateUseCaseBase;
+import com.daemawiki.daemawiki.interfaces.document.dto.request.DocumentElementDtos;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-public interface UpdateDocumentContentsUseCase extends DocumentUpdateUseCaseBase<List<DocumentContent>> {
-    Mono<Void> update(String documentId, List<DocumentContent> contents);
+public interface UpdateDocumentContentsUseCase extends DocumentUpdateUseCaseBase<List<DocumentElementDtos.DocumentContentDto>> {
+    Mono<Void> update(String documentId, List<DocumentElementDtos.DocumentContentDto> updateData);
 }
--- src/main/java/com/daemawiki/daemawiki/application/document/service/UpdateDocumentContentsService.java
+++ src/main/java/com/daemawiki/daemawiki/application/document/service/UpdateDocumentContentsService.java
@@ -2,21 +2,25 @@ package com.daemawiki.daemawiki.application.document.service;
 
 import com.daemawiki.daemawiki.application.document.service.base.AbstractDocumentUpdateServiceBase;
 import com.daemawiki.daemawiki.application.document.UpdateDocumentContentsUseCase;
-import com.daemawiki.daemawiki.domain.document.model.DocumentEntity;
-import com.daemawiki.daemawiki.domain.document.model.detail.DocumentContent;
+import com.daemawiki.daemawiki.domain.document.model.DocumentElementMapper;
 import com.daemawiki.daemawiki.domain.document.repository.DocumentRepository;
 import com.daemawiki.daemawiki.application.user.component.CurrentUser;
+import com.daemawiki.daemawiki.interfaces.document.dto.request.DocumentElementDtos;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Service
-class UpdateDocumentContentsService extends AbstractDocumentUpdateServiceBase<List<DocumentContent>> implements UpdateDocumentContentsUseCase {
+class UpdateDocumentContentsService extends AbstractDocumentUpdateServiceBase<List<DocumentElementDtos.DocumentContentDto>> implements UpdateDocumentContentsUseCase {
 
     @Override
-    public Mono<Void> update(String documentId, List<DocumentContent> contents) {
-        return updateDocument(documentId, contents, DocumentEntity::updateContents);
+    public Mono<Void> update(String documentId, List<DocumentElementDtos.DocumentContentDto> updateData) {
+        return updateDocument(
+                documentId,
+                updateData,
+                (document, contents) -> document.updateContents(DocumentElementMapper.toContentList(contents))
+        );
     }
 
     public UpdateDocumentContentsService(DocumentRepository documentRepository, CurrentUser currentUser) {
