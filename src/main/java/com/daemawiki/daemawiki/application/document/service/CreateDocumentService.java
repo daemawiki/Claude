8f4b43ec2eab8abd81213027bf46487d9528e662
101 <tmddnjs001133@dsm.hs.kr>
2024-08-22 21:47:57 +0900

    refactor: package 이동 및 etc.

--- src/main/java/com/daemawiki/daemawiki/application/document/service/CreateDocumentService.java
+++ src/main/java/com/daemawiki/daemawiki/application/document/service/CreateDocumentService.java
@@ -1,10 +1,10 @@
 package com.daemawiki.daemawiki.application.document.service;
 
-import com.daemawiki.daemawiki.domain.document.model.DocumentElementMapper;
+import com.daemawiki.daemawiki.domain.document.DocumentElementMapper;
+import com.daemawiki.daemawiki.domain.document.DocumentModel;
 import com.daemawiki.daemawiki.interfaces.document.dto.request.CreateDocumentRequest;
-import com.daemawiki.daemawiki.domain.document.model.DefaultDocumentEntityFactory;
-import com.daemawiki.daemawiki.domain.document.model.DocumentEntity;
-import com.daemawiki.daemawiki.domain.document.repository.DocumentRepository;
+import com.daemawiki.daemawiki.domain.document.DefaultDocumentEntityFactory;
+import com.daemawiki.daemawiki.domain.document.DocumentRepository;
 import com.daemawiki.daemawiki.application.document.usecase.CreateDocumentUseCase;
 import com.daemawiki.daemawiki.application.user.component.CurrentUser;
 import com.daemawiki.daemawiki.domain.user.model.UserEntity;
@@ -24,7 +24,7 @@ class CreateDocumentService implements CreateDocumentUseCase {
                 .then();
     }
 
-    private static DocumentEntity createDocumentEntity(CreateDocumentRequest request, UserEntity user) {
+    private static DocumentModel createDocumentEntity(CreateDocumentRequest request, UserEntity user) {
         return DefaultDocumentEntityFactory.createDocumentEntity(
                 request,
                 null,
--- src/main/java/com/daemawiki/daemawiki/application/document/usecase/UpdateDocumentInfoUseCase.java
+++ src/main/java/com/daemawiki/daemawiki/application/document/usecase/UpdateDocumentInfoUseCase.java
@@ -4,6 +4,6 @@ import com.daemawiki.daemawiki.interfaces.document.dto.request.DocumentElementDt
 import com.daemawiki.daemawiki.application.document.usecase.base.DocumentUpdateUseCaseBase;
 import reactor.core.publisher.Mono;
 
-public interface UpdateDocumentInfoUseCase extends DocumentUpdateUseCaseBase<DocumentElementDtos.DocumentInfoUpdateDto> {
-    Mono<Void> update(String documentId, DocumentElementDtos.DocumentInfoUpdateDto request);
+public interface UpdateDocumentInfoUseCase extends DocumentUpdateUseCaseBase<DocumentElementDtos.InfoUpdateDto> {
+    Mono<Void> update(String documentId, DocumentElementDtos.InfoUpdateDto request);
 }
