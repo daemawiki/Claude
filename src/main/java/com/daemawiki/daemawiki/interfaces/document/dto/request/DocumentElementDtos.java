fba9e6514010f4244202194a41dc575d5e41e416
101 <tmddnjs001133@dsm.hs.kr>
2024-08-22 21:44:07 +0900

    refactor: dtos 클래스 구성 관계 변경

--- src/main/java/com/daemawiki/daemawiki/interfaces/document/dto/request/DocumentElementDtos.java
+++ src/main/java/com/daemawiki/daemawiki/interfaces/document/dto/request/DocumentElementDtos.java
@@ -1,25 +1,30 @@
 package com.daemawiki.daemawiki.interfaces.document.dto.request;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
 public class DocumentElementDtos {
-    public record DocumentInfoUpdateDto(
-            List<DocumentDetailDto> detailList,
-            DocumentTitleDto title
-    ) {
-        public record DocumentDetailDto(
-                String title,
-                String content
-        ) {}
 
-        public record DocumentTitleDto(
-                String mainTitle,
-                String subTitle
-        ) {}
+    public enum TypeDto {
+        STUDENT, MAIN, TEACHER, INCIDENT, TEST
     }
+    public record InfoUpdateDto(
+            List<DetailDto> detailList,
+            TitleDto title
+    ) {}
 
-    public record DocumentContentDto(
+    public record DetailDto(
+            String title,
+            String content
+    ) {}
+
+    public record TitleDto(
+            String mainTitle,
+            String subTitle
+    ) {}
+
+    public record ContentDto(
             String index,
             String title,
             String content
@@ -29,5 +34,15 @@ public class DocumentElementDtos {
             String mainTitle,
             String subTitle,
             Set<String> categoryList
+    ) {}
+
+    public record EditorDto(
+            String name,
+            String userId
+    ) {}
+
+    public record EditDatetimeDto(
+            LocalDateTime createdDateTime,
+            LocalDateTime lastModifiedDateTime
     ) {}
 }
