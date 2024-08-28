2aedd2da55420fc712996713817d49cd41d21fd2
101 <tmddnjs001133@dsm.hs.kr>
2024-08-28 20:10:16 +0900

    update: 정적 팩토리 메서드 추가

--- src/main/java/com/daemawiki/daemawiki/interfaces/document/dto/response/FullDocumentResponse.java
+++ src/main/java/com/daemawiki/daemawiki/interfaces/document/dto/response/FullDocumentResponse.java
@@ -1,12 +1,14 @@
 package com.daemawiki.daemawiki.interfaces.document.dto.response;
 
+import com.daemawiki.daemawiki.common.annotation.Factory;
+import com.daemawiki.daemawiki.domain.document.DocumentModel;
+
 import java.util.List;
 import java.util.Set;
 
 public record FullDocumentResponse(
         String id,
-        String title,
-        String subTitle,
+        Title title,
         List<Detail> details,
         List<Content> contents,
         Set<String> categories,
@@ -17,8 +19,32 @@ public record FullDocumentResponse(
         Editor owner,
         List<Editor> editors
 ) {
-    public record EditDateTime(String createdAt, String lastEditedAt) {}
-    public record Detail(String title, String content) {}
-    public record Content(String index, String title, String content) {}
-    public record Editor(String name, String userId) {}
+    @Factory
+    public static FullDocumentResponse createNewInstanceFromDocumentModel(DocumentModel model) {
+        return new FullDocumentResponse(
+                model.id(),
+                new Title(model.title().mainTitle(), model.title().subTitle()),
+                model.detailList().stream()
+                        .map(detail -> new Detail(detail.title(), detail.content()))
+                        .toList(),
+                model.contentList().stream()
+                        .map(content -> new Content(content.index(), content.title(), content.content()))
+                        .toList(),
+                model.categorySet(),
+                model.viewCount(),
+                model.version(),
+                model.type().name(),
+                new EditDateTime(model.dateTime().createdDateTime().toString(), model.dateTime().lastModifiedDateTime().toString()),
+                new Editor(model.owner().name(), model.owner().userId()),
+                model.editorList().stream()
+                        .map(editor -> new Editor(editor.name(), editor.userId()))
+                        .toList()
+        );
+    }
+
+    record Title(String mainTitle, String subTitle) {}
+    record EditDateTime(String createdDateTime, String lastModifiedDateTime) {}
+    record Detail(String title, String content) {}
+    record Content(String index, String title, String content) {}
+    record Editor(String name, String userId) {}
 }
