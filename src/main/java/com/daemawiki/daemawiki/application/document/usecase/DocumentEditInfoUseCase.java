8ae4154ecbd927015eda4e8bb05d7967ba14281b
101 <tmddnjs001133@dsm.hs.kr>
2024-08-30 20:27:12 +0900

    delete: edit info usecase

--- src/main/java/com/daemawiki/daemawiki/application/document/usecase/DocumentEditInfoUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.daemawiki.daemawiki.application.document.usecase;
-
-import com.daemawiki.daemawiki.interfaces.document.dto.DocumentElementDtos;
-import com.daemawiki.daemawiki.application.document.usecase.base.DocumentUpdateUseCaseBase;
-import reactor.core.publisher.Mono;
-
-public interface DocumentEditInfoUseCase extends DocumentUpdateUseCaseBase<DocumentElementDtos.UpdateInfo> {
-    Mono<Void> update(String documentId, DocumentElementDtos.UpdateInfo request);
-}
