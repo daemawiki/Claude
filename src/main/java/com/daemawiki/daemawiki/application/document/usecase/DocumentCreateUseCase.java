33f6da9117071aaf2e6cb0cb0972cf303a8e22ba
101 <tmddnjs001133@dsm.hs.kr>
2024-08-30 20:40:19 +0900

    refactor: 도메인 명이 앞으로 가도록 수정

--- src/main/java/com/daemawiki/daemawiki/application/document/usecase/DocumentCreateUseCase.java
+++ src/main/java/com/daemawiki/daemawiki/application/document/usecase/DocumentCreateUseCase.java
@@ -3,6 +3,6 @@ package com.daemawiki.daemawiki.application.document.usecase;
 import com.daemawiki.daemawiki.interfaces.document.dto.request.CreateDocumentRequest;
 import reactor.core.publisher.Mono;
 
-public interface CreateDocumentUseCase {
+public interface DocumentCreateUseCase {
     Mono<Void> create(CreateDocumentRequest request);
 }
