0768e2d007452b9e7e25f0bc5bd97b3c547ebb45
101 <tmddnjs001133@dsm.hs.kr>
2024-08-30 20:27:20 +0900

    add: edit usecase

---
+++ src/main/java/com/daemawiki/daemawiki/application/document/usecase/DocumentEditUseCase.java
@@ -0,0 +1,12 @@
+package com.daemawiki.daemawiki.application.document.usecase;
+
+import com.daemawiki.daemawiki.interfaces.document.dto.DocumentElementDtos;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public interface DocumentEditUseCase {
+    Mono<Void> editInfo(String documentId, DocumentElementDtos.UpdateInfo request);
+    Mono<Void> editEditors(String documentId, List<DocumentElementDtos.Editor> updateData);
+    Mono<Void> editContents(String documentId, List<DocumentElementDtos.Content> updateData);
+}
