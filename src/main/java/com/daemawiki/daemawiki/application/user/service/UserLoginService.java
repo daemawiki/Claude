d16fea188042714cb9876e9b472b82254eafc6e6
101 <tmddnjs001133@dsm.hs.kr>
2024-08-31 14:21:45 +0900

    refactor: 주석 삭제 및 메서드 명 올바르게 변경

--- src/main/java/com/daemawiki/daemawiki/application/user/service/UserLoginService.java
+++ src/main/java/com/daemawiki/daemawiki/application/user/service/UserLoginService.java
@@ -23,13 +23,13 @@ import java.net.InetSocketAddress;
 class UserLoginService implements UserLoginUseCase {
 
     @Override
-    public Mono<String> loginWithSession(UserLoginRequest request, ServerHttpRequest serverHttpRequest) {
+    public Mono<String> login(UserLoginRequest request, ServerHttpRequest serverHttpRequest) {
         return Mono.justOrEmpty(serverHttpRequest.getRemoteAddress())
                 .switchIfEmpty(Mono.error(new RuntimeException("너 누구야!!")))
-                .flatMap(remoteAddress -> createSessionAndLogin(request, remoteAddress));
+                .flatMap(remoteAddress -> createSession(request, remoteAddress));
     }
 
-    private Mono<String> createSessionAndLogin(UserLoginRequest request, InetSocketAddress remoteAddress) {
+    private Mono<String> createSession(UserLoginRequest request, InetSocketAddress remoteAddress) {
         return loginProcess(request)
                 .map(user -> SessionModel.of(remoteAddress.toString(), user.getEmail()))
                 .flatMap(sessionRepository::save)
@@ -42,13 +42,6 @@ class UserLoginService implements UserLoginUseCase {
                 .flatMap(user -> validatePassword(user, request.password()));
     }
 
-    /**
-     * 비밀번호 검증 및 토큰 생성
-     *
-     * @param user DB에서 조회된 사용자 엔티티
-     * @param requestPassword 로그인 요청에서 제공된 비밀번호
-     * @return 인증 성공 시 사용자 엔티티와 생성된 토큰을 묶은 Tuple2, 비밀번호 불일치 시 Mono error signal
-     */
     private Mono<UserEntity> validatePassword(UserEntity user, String requestPassword) {
         return passwordEncoder.matches(requestPassword, user.getPassword())
                 ? Mono.just(user)
