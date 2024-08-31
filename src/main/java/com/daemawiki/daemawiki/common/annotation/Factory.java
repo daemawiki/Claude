46ff3ffebec6aa2aa5e764bf59cdce99f8b8646f
101 <tmddnjs001133@dsm.hs.kr>
2024-08-31 13:28:43 +0900

    update: target 추가

--- src/main/java/com/daemawiki/daemawiki/common/annotation/Factory.java
+++ src/main/java/com/daemawiki/daemawiki/common/annotation/Factory.java
@@ -4,6 +4,6 @@ import java.lang.annotation.*;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.METHOD })
+@Target({ ElementType.METHOD, ElementType.TYPE })
 public @interface Factory {
 }
