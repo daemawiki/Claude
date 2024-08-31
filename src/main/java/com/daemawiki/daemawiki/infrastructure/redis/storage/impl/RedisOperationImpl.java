49fe7cdd728a0fd16a7cdc5bd2bc71bbe16b8ae5
101 <tmddnjs001133@dsm.hs.kr>
2024-08-31 14:58:31 +0900

    refactor: 의존성 상단으로 올리기

--- src/main/java/com/daemawiki/daemawiki/infrastructure/redis/storage/impl/RedisOperationImpl.java
+++ src/main/java/com/daemawiki/daemawiki/infrastructure/redis/storage/impl/RedisOperationImpl.java
@@ -11,6 +11,8 @@ import java.time.Duration;
 @Component
 @RequiredArgsConstructor
 class RedisOperationImpl implements RedisOperation {
+    private final ReactiveRedisOperations<String, String> redisOperations;
+
     @Override
     public Mono<String> getValue(String key) {
         return redisOperations.opsForValue().get(key);
@@ -30,7 +32,5 @@ class RedisOperationImpl implements RedisOperation {
     public Mono<Boolean> save(String key, String value, Duration expiration) {
         return redisOperations.opsForValue().set(key, value, expiration);
     }
-
-    private final ReactiveRedisOperations<String, String> redisOperations;
 }
 
