b7638db75de549e48d20e57c0499a09ff0ec232d
101 <tmddnjs001133@dsm.hs.kr>
2024-08-31 14:58:09 +0900

    refactor: 의존성 위로 올리기

--- src/main/java/com/daemawiki/daemawiki/application/mail/service/UserMailSendService.java
+++ src/main/java/com/daemawiki/daemawiki/application/mail/service/UserMailSendService.java
@@ -17,6 +17,19 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 @Slf4j(topic = "유저 메일 전송 서비스")
 class UserMailSendService implements UserMailSendUseCase {
+    private static final String MAIL_TEMPLATE =
+            "<div style='margin: 10px; background-color: #f5f5f5; padding: 20px; border-radius: 10px;'>"
+                    + "<p style='font-size: 16px; color: #333;'><b><span style='color: #007bff;'>D</span><span style='color: #ffcc00;'>S</span><span style='color: #ff0000;'>M</span></b> 이메일 인증 코드 :</p>"
+                    + "<p style='font-size: 24px; font-weight: bold; color: #007bff; letter-spacing: 3px;'>%s</p>"
+                    + "<p style='font-size: 14;font-style: italic; color: #999;'>인증 코드는 30분 동안 유효합니다.</p>"
+                    + "</div>";
+    private static final int CODE_LENGTH = 6;
+
+    private final ApplicationEventPublisher eventPublisher;
+    private final AuthCodeRepository authCodeRepository;
+    private final AuthCodeGenerator authCodeGenerator;
+    private final UserRepository userRepository;
+
     @Override
     public Mono<Void> send(String to, MailType type) {
         return userRepository.findByEmail(to)
@@ -39,18 +52,4 @@ class UserMailSendService implements UserMailSendUseCase {
                 .thenReturn(authCodeModel)
                 .onErrorMap(e -> e);
     }
-
-    private static final int CODE_LENGTH = 6;
-    private static final String MAIL_TEMPLATE =
-            "<div style='margin: 10px; background-color: #f5f5f5; padding: 20px; border-radius: 10px;'>"
-                    + "<p style='font-size: 16px; color: #333;'><b><span style='color: #007bff;'>D</span><span style='color: #ffcc00;'>S</span><span style='color: #ff0000;'>M</span></b> 이메일 인증 코드 :</p>"
-                    + "<p style='font-size: 24px; font-weight: bold; color: #007bff; letter-spacing: 3px;'>%s</p>"
-                    + "<p style='font-size: 14;font-style: italic; color: #999;'>인증 코드는 30분 동안 유효합니다.</p>"
-                    + "</div>";
-
-
-    private final ApplicationEventPublisher eventPublisher;
-    private final AuthCodeRepository authCodeRepository;
-    private final AuthCodeGenerator authCodeGenerator;
-    private final UserRepository userRepository;
 }
