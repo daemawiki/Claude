7f08a1a454cf78238388ac1fa44b4823467c60e0
101 <tmddnjs001133@dsm.hs.kr>
2024-08-31 14:58:42 +0900

    refactor: 의존성 상단으로 올리기

--- src/main/java/com/daemawiki/daemawiki/domain/mail/repository/impl/AuthCodeRepositoryImpl.java
+++ src/main/java/com/daemawiki/daemawiki/domain/mail/repository/impl/AuthCodeRepositoryImpl.java
@@ -17,6 +17,10 @@ import java.time.Duration;
 @RequiredArgsConstructor
 @Slf4j(topic = "메일 인증 코드 레디스 레포지토리")
 class AuthCodeRepositoryImpl implements AuthCodeRepository {
+    private static final String AUTH_CODE = RedisKey.AUTH_CODE.getKey();
+
+    private final RedisOperation redisOperation;
+
     @Override
     public Mono<Boolean> save(AuthCodeModel model) {
         return handleError(redisOperation.save(
@@ -41,14 +45,11 @@ class AuthCodeRepositoryImpl implements AuthCodeRepository {
     public Mono<Long> deleteByEmail(String email) {
         return handleError(redisOperation.delete(AUTH_CODE + email));
     }
+
     private static <T> Mono<T> handleError(Mono<T> mono) {
         return mono.onErrorMap(e -> {
             log.error("#- Error: " + e);
             return e instanceof RedisConnectionFailureException ? WrongRedisConnectionException.EXCEPTION : e;
         });
     }
-
-
-    private static final String AUTH_CODE = RedisKey.AUTH_CODE.getKey();
-    private final RedisOperation redisOperation;
 }
